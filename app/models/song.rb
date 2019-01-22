5ec885f26fd650a7214d4806ce051edc61495c6e
Sean Langton <slangton1@gmail.com>
2019-01-22 03:50:52 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,6 +7,8 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    self.artist = Artist.find_by(name: "Drake")
+    drake = Artist.find_by(name: "Drake")
+    binding.pry
+    self.artist = drake
   end
 end
